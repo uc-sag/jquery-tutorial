d1503c7bfd698cfd8158b156d72c85960820fab6
uc-sag <sagar.gupta@ucertify.com>
2023-08-02 09:43:03 +0530

    Basic syntax css selectors

--- index.php
+++ index.php
@@ -7,9 +7,39 @@
     <title>Document</title>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script>
+//     $(document).ready(function(){
+//         alert('doc loaded');
+
+// });
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+$(document).ready(function(){
+  $("#test").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".test").click(function(){
+    $(".test").hide();
+  });
+});
+</script>
 
 </head>
 <body>
-    <h2>Hello Jquery</h2>
+    <h2 id="test">Hello Jquery</h2>
+    <p>Para 1</p>
+    <p>Para 2</p>
+    <p>Para 3</p>
+
+    <span class="test">dsfsda</span>
+    <span class="test">dsfsdasdfasdfad</span>
+    <span class="test">dsfsdasdfasdfaddsfad</span>
+    
 </body>
 </html>
